61aec85c3c654fc69744f4a31670b4a8a0711de2
Adam <adam.l.sosa@gmail.com>
2022-04-08 18:51:04 -0400

    phase-1-control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  console.log(distance)
+  
+
+  if (distance <= 400){
+    return "This one is on me!"
+  } else if (distance > 2000 && distance <=2500) {
+    return 'I will gladly take your thirty bucks'
+  } else if (distance >2500) {
+    return "No can do"
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  city === "NYC" ? "Ok,sounds good," : "No go"
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+    return "thank you so much"
+    case "not as generous":
+    return "thank you"
+    default:
+      
+    return "bye"
+  }
 }
