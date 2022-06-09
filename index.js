6f123c9ffc70cf9451fe491f0ae27d8463c92191
Adam <adam.l.sosa@gmail.com>
2022-06-09 13:47:59 -0400

    index.js

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-function scuberGreetingForFeet(distance){
-  // Write your code here!
-  console.log(distance)
-  
 
-  if (distance <= 400){
-    return "This one is on me!"
-  } else if (distance > 2000 && distance <=2500) {
-    return 'I will gladly take your thirty bucks'
-  } else if (distance >2500) {
-    return "No can do"
+function scuberGreetingForFeet(distance){
+  if (distance < 400) {
+    return 'This one is on me!';
+  }else if (distance < 2000) {
+    return 'That will be twenty bucks.'; 
+  }if (distance < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }else if (distance > 2500){
+  return 'No can do.';
+  }
 }
 
 function ternaryCheckCity(city){
-  city === "NYC" ? "Ok,sounds good," : "No go"
+return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(tip){
-  switch (tip) {
-    case "generous":
-    return "thank you so much"
-    case "not as generous":
-    return "thank you"
-    default:
-      
-    return "bye"
+function switchOnCharmFromTip(charm){
+
+  switch (charm) {
+    case 'generous':
+     return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    case 'thanks for everything':
+      return 'Bye.';
   }
-}
+
+
+  }
