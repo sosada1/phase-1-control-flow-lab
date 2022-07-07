4f4a4c0a73cd34021c1ed3531bbb90e1e96f8405
Adam <adam.l.sosa@gmail.com>
2022-07-07 19:09:20 -0400

    commit tree 6c8fe238191c0d06993485d517847e404d094c0b

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 
-function scuberGreetingForFeet(distance){
-  if (distance < 400) {
-    return 'This one is on me!';
-  }else if (distance < 2000) {
-    return 'That will be twenty bucks.'; 
-  }if (distance < 2500){
-    return 'I will gladly take your thirty bucks.';
-  }else if (distance > 2500){
-  return 'No can do.';
-  }
+function scuberGreetingForFeet(ride){
+if (ride < 400) {
+  return 'This one is on me!'
+} else if (ride > 2500){
+  return 'No can do.'
+} else if ( ride > 2000){
+  return 'I will gladly take your thirty bucks.'
+} else {
+  return 'That will be twenty bucks.'
 }
-
+}
 function ternaryCheckCity(city){
-return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+ return city === 'NYC'? 'Ok, sounds good.' : 'No go.' 
 }
+function switchOnCharmFromTip(tip){ 
 
-function switchOnCharmFromTip(charm){
-
-  switch (charm) {
-    case 'generous':
-     return 'Thank you so much.';
-    case 'not as generous':
-      return 'Thank you.';
-    case 'thanks for everything':
-      return 'Bye.';
-  }
-
-
-  }
+switch (tip) {
+  case 'generous':
+    return 'Thank you so much.';
+    break;
+  case 'not as generous': 
+    return  'Thank you.'; 
+    break;
+  default:
+  return 'Bye.';
+  }}
